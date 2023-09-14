93643ce330e08944f0f9cc7fb5713c23d070ff5d
Habeeb Moosa <habeebmoosadev@gmail.com>
2023-09-14 22:49:00 +0530

    MongoDB database is connected with this blog

--- api/server.js
+++ api/server.js
@@ -1,12 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
-const PORT = 3001;
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('MongoDB connected...')
+}).catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port.`);
 });
