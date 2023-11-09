55b2f46d6511a676f614630d9a26e253c7c999ff
Habeeb Moosa <habeebmoosadev@gmail.com>
2023-11-09 22:08:23 +0530

    Contacts and feedbac can only accessable to owners

--- api/controller/admin/contact.controller.js
+++ api/controller/admin/contact.controller.js
@@ -1,7 +1,14 @@
+import { Author } from "../../models/author.model.js";
 import { Contact } from "../../models/pageModels/contact.model.js";
 
 export const readContactInfo = async (req, res) => {
+    const id = req.userId;
     try {
+        const isOwner = await Author.findOne({ _id: id, position: "owner" });
+        if (!isOwner) {
+            return res.status(200).json({ message: "You are not authorized to access contacts", position: "author" });
+        }
+
         const contactInfo = await Contact.find();
         res.status(200).json(contactInfo);
     } catch (error) {
--- api/routes/contact.route.js
+++ api/routes/contact.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { sendContactInfo } from '../controller/public/contact.controller.js';
 import { readContactInfo } from '../controller/admin/contact.controller.js';
+import { verifyAuth } from '../middlewares/verifyAuth.js';
 
 const router = express.Router();
 
 router.post('/create', sendContactInfo);
-router.get('/read', readContactInfo);
+router.get('/read', verifyAuth, readContactInfo);
 
 export { router as contactRoutes }
