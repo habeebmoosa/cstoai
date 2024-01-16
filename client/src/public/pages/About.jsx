8866f6c0f9bf6f3507999bac2e79591d73cf8847
Habeeb Moosa <habeebmoosadev@gmail.com>
2024-01-16 19:51:34 +0530

    Update About page content

--- client/src/public/pages/About.jsx
+++ client/src/public/pages/About.jsx
@@ -6,10 +6,7 @@ export const About = () => {
             <div className="left w-full lg:w-1/2 lg:max-w-md flex flex-col justify-center space-y-4">
                 <h1 className="text-4xl font-bold text-gray-800">About</h1>
                 <p className="text-gray-600">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Quaerat, quod.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Quaerat, quod.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Quaerat, quod.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Quaerat, quod.
+                    This is my blog page where you can find stuffs related to web development. I am a full stack web developer and I love to share my knowledge with others. I am a student and I am currently pursuing my B.Tech degree in Computer Science and Engineering. In this blog app you can get one of the most important and usefull blog posts related to computer science and web development. I hope you will like it.
                 </p>
             </div>
             <div className="right w-full lg:w-1/2">
